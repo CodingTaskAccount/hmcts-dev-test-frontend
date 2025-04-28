ee7cf1c176ba06d5b07df431257a32fd1565dd7d
North <lewis.north99+github@gmail.com>
2025-04-28 01:03:36 +0100

    Set up npm

---
+++ src/main/routes/home.ts
@@ -0,0 +1,19 @@
+import { Application } from 'express';
+import axios from 'axios';
+
+export default function (app: Application): void {
+  app.get('/', async (req, res) => {
+    try {
+      // An example of connecting to the backend (a starting point)
+      const response = await axios.get('http://localhost:4000/retrieveTask/1');
+      if (response.status === 200) {
+        res.render('home', { "example": response.data, isFound: true });
+      } else {
+        res.render('home', { "example": response.data, isFound: false})
+      }
+    } catch (error) {
+      console.error('Error making request:', error);
+      res.render('home', {});
+    }
+  });
+}
