a4e4376bd903160d47408ab6a8d0e812b1ceaf36
North <lewis.north99@gmail.com>
2025-04-28 14:02:03 +0100

    Set up task display component

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -5,11 +5,11 @@ export default function (app: Application): void {
   app.get('/', async (req, res) => {
     try {
       // An example of connecting to the backend (a starting point)
-      const response = await axios.get('http://localhost:4000/retrieveTask/1');
+      const response = await axios.get('http://localhost:4000/retrieveAllTasks');
       if (response.status === 200) {
-        res.render('home', { "example": response.data, isFound: true });
+        res.render('home', { "tasks": response.data, isOk: true });
       } else {
-        res.render('home', { "example": response.data, isFound: false})
+        res.render('home', { "tasks": response.data, isOk: false})
       }
     } catch (error) {
       console.error('Error making request:', error);
